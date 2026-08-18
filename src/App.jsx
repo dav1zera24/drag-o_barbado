9eaa2eaff5d118503637d8bc6b35e3f84d64cf85
gbonettodev <bonettogustavo19@gmail.com>
2026-08-18 11:24:06 -0300

    213243

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Sobre from "./Pages/Sobre";
- contato
 import Contato from "./Pages/Contato";
 
 import Produtos from "./Pages/Produtos";
 import FAQ from "./Pages/FAQ";
-leleo3
 import Notfound from "./Pages/Notfound";
-main
-main
 
 function App() {
   return (
@@ -19,10 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
-leleo3
+
         <Route path="/contato" element={<Contato />} />
         <Route path="*" element={<Home />} />
-main
         <Route path="/produtos" element={<Produtos />} />
         <Route path="/FAQ" element={<FAQ />} />
         <Route path="/notfound" element={<Notfound />} />
